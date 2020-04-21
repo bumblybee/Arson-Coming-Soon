e4abd4884a0151a58ebbb620dba8dde4b72f1af2
Tiffani Hess <hesstjune@gmail.com>
2020-04-21 16:26:18 -0500

    added way to hide form after submission to prevent someone from making bogus submissions multiple times. Also added way to unlock form/clear local storage for testing purposes

---
+++ index.js
@@ -0,0 +1,48 @@
+const signupButton = document.getElementById("signup-button");
+const form = document.getElementById("signup-form");
+const thanks = document.getElementById("thanks");
+
+const pearson = document.getElementById("pearson");
+
+let clicks = 0;
+
+form.addEventListener("submit", (e) => {
+  //when submitting form, set submitted to true in local storage
+  // e.preventDefault();
+  localStorage.setItem("submitted", true);
+
+  //if submitted is true in local storage, remove form and add thanks for signing up
+  if (JSON.parse(localStorage.getItem("submitted"))) {
+    console.log("added");
+    form.classList.add("hidden");
+    thanks.classList.remove("hidden");
+  }
+});
+
+//Secret way to clear local storage and unlock form again
+pearson.addEventListener("click", (e) => {
+  clicks++;
+  if (clicks >= 5) {
+    localStorage.clear();
+    form.classList.remove("hidden");
+    thanks.classList.add("hidden");
+    clicks = 0;
+  }
+});
+
+// let submitCount = 0;
+// let count = [];
+
+// signupButton.addEventListener("click", (e) => {
+//   submitCount++;
+//   count.push(submitCount);
+
+//   if (count.length > 10) {
+//     const form = document.getElementById("signup-form");
+//     form.classList.add("hidden");
+//   }
+
+//   console.log(submitCount);
+//   console.log(count);
+//   e.preventDefault();
+// });
